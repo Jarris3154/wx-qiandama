9d98c9bdd8772c51b84e3140282cd7048cc3f64f
Jarris3154 <hu.jia.jarris@qq.com>
2020-07-15 19:44:26 +0800

    refactor: adjust algothriom

--- pages/qiandama/qiandama.js
+++ pages/qiandama/qiandama.js
@@ -116,7 +116,7 @@ Page({
       })
   },
   getDiscount: function () {
-    let discount = 1;
+    let discount = 10;
 
     let now = new Date();
 
@@ -133,14 +133,14 @@ Page({
 
     let diffMillis = now.getTime() - today19.getTime();
 
-    if (now > today19) {
+    if (now >= today19) {
+      console.log(diffMillis / period30min)
       //因为19点开始要减1，所以要多减掉1折。
       let step = parseInt(diffMillis / period30min) + 1;
       discount -= step;
-      discount = discount / 10;
     }
 
-    return discount;
+    return discount / 10;
   },
   multiply: function (arg1, arg2) {
     var m = 0,
